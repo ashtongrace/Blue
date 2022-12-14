1887fde3452389256c1eb5c3572ee08ce31ceb14
Tim Way <1091435+timway@users.noreply.github.com>
2022-12-14 01:28:18 -0600

    Updates to sleep to include persisting data

--- app/src/main/java/uwgb/cs292/blue/sleep.java
+++ app/src/main/java/uwgb/cs292/blue/sleep.java
@@ -1,76 +1,88 @@
 package uwgb.cs292.blue;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.Objects;
+import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link sleep#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class sleep extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int today = 0;
 
     public sleep() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment sleep.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static sleep newInstance(String param1, String param2) {
-        sleep fragment = new sleep();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sleep, container, false);
     }
 
-    //public void sleepyHours() {
-        //When button is pressed, hours will be passed in
-        //Int a = ((EditText) findViewById(R.id.editSleepHours));
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Calendar now = Calendar.getInstance();
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH);
+        int year = now.get(Calendar.YEAR);
 
-        //save the sleepy hours home skillet
-       // }
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        today = preferences.getInt(
+            "sleep-" + String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.valueOf(day),
+            0);
+
+        final EditText s = (EditText) view.findViewById(R.id.editSleepHours);
+        if (today > 0) {
+            s.setText(String.valueOf(today));
+        }
+
+        final Button b = (Button) view.findViewById(R.id.sleepyButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("sleep-onclick", "clicked");
+                final EditText s = (EditText) view.findViewById(R.id.editSleepHours);
+                today = Integer.parseInt(s.getText().toString());
+            }
+        });
     }
 
+    @Override
+    public void onStop () {
+        super.onStop();
+
+        Calendar now = Calendar.getInstance();
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH);
+        int year = now.get(Calendar.YEAR);
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        if ( today > 0 ) {
+            editor.putInt(
+            "sleep-" + String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.valueOf(day),
+                today
+            );
+        }
+        editor.apply();
+
+        Log.e("on-stop", "today - " + String.valueOf(today));
+    }
+}
