244b13de5804fd0b2f5e6ac5d5250e285b8568e6
Tim Way <1091435+timway@users.noreply.github.com>
2022-12-14 02:23:42 -0600

    Finalized data persistence

--- app/src/main/java/uwgb/cs292/blue/eating.java
+++ app/src/main/java/uwgb/cs292/blue/eating.java
@@ -1,64 +1,129 @@
 package uwgb.cs292.blue;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
-import java.util.Date;
+import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link eating#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class eating extends Fragment {
-    private Date breakfast, lunch, dinner;
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int breakfast = 0, lunch = 0, dinner = 0;
 
     public eating() {
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment eating.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static eating newInstance(String param1, String param2) {
-        eating fragment = new eating();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        breakfast = preferences.getInt("eating-breakfast", 0);
+        lunch = preferences.getInt("eating-lunch", 0);
+        dinner = preferences.getInt("eating-dinner", 0);
+
+        Log.e("on-create", "brk - " + String.valueOf(breakfast) + " lnh - " + String.valueOf(lunch) + " din - " + String.valueOf(dinner));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_eating, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Calendar now = Calendar.getInstance();
+        int today = now.get(Calendar.DAY_OF_MONTH);
+        Log.e("Date Math", "Today is " + today);
+
+        final CheckBox b = (CheckBox) view.findViewById(R.id.breakfast_cb);
+        final CheckBox l = (CheckBox) view.findViewById(R.id.lunch_cb);
+        final CheckBox d = (CheckBox) view.findViewById(R.id.dinner_cb);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                int today = now.get(Calendar.DAY_OF_MONTH);
+                Log.e("Date Math", "Today is " + today);
+
+                final CheckBox b = (CheckBox) view.findViewById(R.id.breakfast_cb);
+                if (b.isChecked()) {
+                    breakfast = today;
+                } else {
+                    breakfast = 0;
+                }
+            }
+        });
+
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                int today = now.get(Calendar.DAY_OF_MONTH);
+                Log.e("Date Math", "Today is " + today);
+
+                final CheckBox l = (CheckBox) view.findViewById(R.id.lunch_cb);
+                if (l.isChecked()) {
+                    lunch = today;
+                } else {
+                    lunch = 0;
+                }
+            }
+        });
+
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                int today = now.get(Calendar.DAY_OF_MONTH);
+                Log.e("Date Math", "Today is " + today);
+
+                final CheckBox d = (CheckBox) view.findViewById(R.id.dinner_cb);
+                if (d.isChecked()) {
+                    dinner = today;
+                } else {
+                    dinner = 0;
+                }
+            }
+        });
+
+        if (breakfast == today)
+        {
+            b.setChecked(true);
+        }
+        if (lunch == today)
+        {
+            l.setChecked(true);
+        }
+        if (dinner == today)
+        {
+            d.setChecked(true);
+        }
+    }
+
+    @Override
+    public void onStop () {
+        super.onStop();
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putInt("eating-breakfast", breakfast);
+        editor.putInt("eating-lunch", lunch);
+        editor.putInt("eating-dinner", dinner);
+        editor.apply();
+
+        Log.e("on-stop", "brk - " + String.valueOf(breakfast) + " lnh - " + String.valueOf(lunch) + " din - " + String.valueOf(dinner));
     }
 }
--- app/src/main/java/uwgb/cs292/blue/medication.java
+++ app/src/main/java/uwgb/cs292/blue/medication.java
@@ -1,64 +1,120 @@
 package uwgb.cs292.blue;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link medication#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.HashSet;
+
+
 public class medication extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    HashSet<String> medications = new HashSet<String>();
+    HashMap<String, Integer> amounts = new HashMap<String, Integer>();
 
     public medication() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment medication.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static medication newInstance(String param1, String param2) {
-        medication fragment = new medication();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        medications = (HashSet<String>) preferences.getStringSet("medication-names", new HashSet<String>());
+
+        int amount = 0;
+
+        for (String medication : medications) {
+            amount = preferences.getInt("medication-" + medication, 0);
+            amounts.put(medication, amount);
+            Log.d("medications-oncreated", "The " + medication + " with an amount of " + String.valueOf(amount) + " was stored previously");
         }
+
+        Log.d("medications-oncreated", "The medications HashSet has " + String.valueOf(medications.size()));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_medication, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        for (String name : amounts.keySet()) {
+            TextView t = (TextView) getActivity().findViewById(R.id.medication_list);
+            t.setLines(amounts.size() + 1);
+            t.append(name + " : " + String.valueOf(amounts.get(name)) + System.getProperty("line.separator"));
+        }
+
+        final Button b = (Button) view.findViewById(R.id.medication_enter);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("medication-onclick", "clicked");
+
+                String name = ((EditText) view.findViewById(R.id.medication_name)).getText().toString();
+                if (name.isEmpty()) {
+                    return;
+                }
+
+                try {
+                    Integer amount = Integer.parseInt(
+                            ((EditText) view.findViewById(R.id.medication_amount)).getText().toString()
+                    );
+                    amounts.put(name, amount);
+                    TextView t = (TextView) view.findViewById(R.id.medication_list);
+                    t.setLines(amounts.size() + 1);
+                    t.append(name + " : " + String.valueOf(amount) + System.getProperty("line.separator"));
+                } catch (Exception e) {
+                    return;
+                }
+
+                if (medications.add(name)) {
+                    Log.d("medications-onclick", "this medication was not in the list before, it was added");
+                } else {
+                    Log.d("medications-onclick", "this medication was found in the list before, it was not re-added");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStop () {
+        super.onStop();
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putStringSet("medication-names", medications);
+        for (String name : amounts.keySet()) {
+            editor.putInt("medication-" + name, amounts.get(name));
+        }
+        editor.apply();
+
+        Log.e("on-stop", "applied medications hashset");
+    }
+
+    public void medlist() {
+        TextView t = (TextView) getActivity().findViewById(R.id.medication_list);
+        String b = "";
+
+        t.setLines(amounts.size() + 1);
+        for (String name : amounts.keySet()) {
+            b = b + name + " : " + String.valueOf(amounts.get(name)) + System.getProperty("line.separator");
+        }
     }
 }
